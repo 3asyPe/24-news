665d8ffa4b6aeb8d533a5de79db810c0f3994de5
Harefda <harefda@mail.ru>
2021-05-05 18:58:34 +0300

    made unique posts

--- src/app/settings.py
+++ src/app/settings.py
@@ -73,6 +73,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app.wsgi.application'
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 4
+}
+
+
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
--- src/tag/models.py
+++ src/tag/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     class Meta:
         verbose_name = ("Tag")
